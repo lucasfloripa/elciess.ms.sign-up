74616867dd02514203925f3e791df4cd91395c13
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-19 14:48:13 -0300

    refactor: remove unnecessary complexity

--- src/data/usecases/db-register-user.ts
+++ src/data/usecases/db-register-user.ts
@@ -11,14 +11,11 @@ export class DbRegisterUser implements RegisterUser {
   async register (registerUserParams: RegisterUser.Params): Promise<boolean> {
     const { email, password } = registerUserParams
     const exist = await this.loadUserByEmailRepository.loadByEmail(email)
+    let isValid = false
     if (!exist) {
       const hashedPassword = await this.hasher.hash(password)
-      const user = await this.registerUserRepository.register({ email, password: hashedPassword })
-      if (!user) {
-        return false
-      }
-      return true
+      isValid = await this.registerUserRepository.register({ email, password: hashedPassword })
     }
-    return false
+    return isValid
   }
 }
