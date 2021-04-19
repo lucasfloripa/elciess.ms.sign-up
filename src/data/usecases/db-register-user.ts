837b05c9f476f4b41bbc6db2f0249b9663770de2
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-19 14:18:07 -0300

    feat: ensure DbRegisterUser calls hasher with correct value

--- src/data/usecases/db-register-user.ts
+++ src/data/usecases/db-register-user.ts
@@ -1,15 +1,18 @@
 import { RegisterUser } from '@/domain/usecases'
-import { LoadUserByEmailRepository, RegisterUserRepository } from '@/data/protocols'
+import { LoadUserByEmailRepository, RegisterUserRepository, Hasher } from '@/data/protocols'
 
 export class DbRegisterUser implements RegisterUser {
   constructor (
     private readonly registerUserRepository: RegisterUserRepository,
-    private readonly loadUserByEmailRepository: LoadUserByEmailRepository
+    private readonly loadUserByEmailRepository: LoadUserByEmailRepository,
+    private readonly hasher: Hasher
   ) {}
 
   async register (registerUserParams: RegisterUser.Params): Promise<boolean> {
-    const userByEmail = await this.loadUserByEmailRepository.loadByEmail(registerUserParams.email)
-    if (!userByEmail) {
+    const { email, password } = registerUserParams
+    const exist = await this.loadUserByEmailRepository.loadByEmail(email)
+    if (!exist) {
+      await this.hasher.hash(password)
       const user = await this.registerUserRepository.register(registerUserParams)
       if (!user) {
         return false
