af70f6b3768b3b87815b191ca88a73c060ec75aa
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-20 09:36:39 -0300

    test: ensure SignUpController calls registerUser with correct values

--- tests/presentation/controllers/sign-up-controller.spec.ts
+++ tests/presentation/controllers/sign-up-controller.spec.ts
@@ -1,6 +1,7 @@
 import { Controller, Validation } from '@/presentation/protocols'
 import { SignUpController } from '@/presentation/controllers'
 import { badRequest } from '@/presentation/helpers'
+import { RegisterUser } from '@/domain/usecases'
 import { mockValidationStub } from '@/tests/presentation/mocks'
 
 const mockRequest = (): SignUpController.Params => ({
@@ -9,15 +10,26 @@ const mockRequest = (): SignUpController.Params => ({
   passwordConfirmation: 'any_password'
 })
 
+const mockRegisterUserStub = (): RegisterUser => {
+  class RegisterUserStub implements RegisterUser {
+    async register (registerUserParams: RegisterUser.Params): Promise<boolean> {
+      return await Promise.resolve(true)
+    }
+  }
+  return new RegisterUserStub()
+}
+
 type SutTypes = {
   sut: Controller
   validationStub: Validation
+  registerUserStub: RegisterUser
 }
 
 const makeSut = (): SutTypes => {
   const validationStub = mockValidationStub()
-  const sut = new SignUpController(validationStub)
-  return { sut, validationStub }
+  const registerUserStub = mockRegisterUserStub()
+  const sut = new SignUpController(validationStub, registerUserStub)
+  return { sut, validationStub, registerUserStub }
 }
 
 describe('SignUp Controller', () => {
@@ -33,5 +45,15 @@ describe('SignUp Controller', () => {
     jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(badRequest(new Error()))
+  })
+
+  test('Should call RegisterUser with correct values', async () => {
+    const { sut, registerUserStub } = makeSut()
+    const registerSpy = jest.spyOn(registerUserStub, 'register')
+    await sut.handle(mockRequest())
+    expect(registerSpy).toHaveBeenCalledWith({
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
   })
 })
