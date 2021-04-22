fee472ca169ddc4466fc1f9dfbde602e4e36fc66
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-22 16:40:43 -0300

    feat: ensure EmailValidatorAdapter return false if validators returns false

--- src/infra/validators/email-validator-adapter.ts
+++ src/infra/validators/email-validator-adapter.ts
@@ -3,7 +3,6 @@ import validator from 'validator'
 
 export class EmailValidatorAdapter implements EmailValidator {
   isValid (email: string): boolean {
-    validator.isEmail(email)
-    return null
+    return validator.isEmail(email)
   }
 }
