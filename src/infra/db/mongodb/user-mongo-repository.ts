7f9a773657b11e1e366f5a5369510bc16fcf90ff
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-23 10:46:06 -0300

    feat: ensure UserMongoRepository return true if user was found by email

--- src/infra/db/mongodb/user-mongo-repository.ts
+++ src/infra/db/mongodb/user-mongo-repository.ts
@@ -1,10 +1,23 @@
-import { RegisterUserRepository } from '@/data/protocols'
+import { RegisterUserRepository, CheckUserByEmailRepository } from '@/data/protocols'
 import { MongoHelper } from './mongo-helper'
 
-export class UserMongoRepository implements RegisterUserRepository {
+export class UserMongoRepository implements RegisterUserRepository, CheckUserByEmailRepository {
   async register (data: RegisterUserRepository.Params): Promise<boolean> {
     const userCollection = await MongoHelper.getCollection('users')
     const result = await userCollection.insertOne(data)
     return result.ops[0] !== null
+  }
+
+  async checkByEmail (email: string): Promise<boolean> {
+    const userCollection = await MongoHelper.getCollection('users')
+    const user = await userCollection.findOne(
+      { email },
+      {
+        projection: {
+          _id: 1
+        }
+      }
+    )
+    return user !== null
   }
 }
