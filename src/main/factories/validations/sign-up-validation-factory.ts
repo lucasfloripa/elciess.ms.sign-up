bcd6779b136da01e74dcf9bd82286e229f43db0d
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-23 14:52:34 -0300

    feat: ensure SignUpValidation Factory calls ValidationComposite with all validators

---
+++ src/main/factories/validations/sign-up-validation-factory.ts
@@ -0,0 +1,13 @@
+import { Validation } from '@/presentation/protocols'
+import { ValidationComposite, RequiredFieldValidation, CompareFieldsValidation, EmailValidation } from '@/utils/validators'
+import { EmailValidatorAdapter } from '@/infra/validators'
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['email', 'password', 'passwordConfirmation']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
+  return new ValidationComposite(validations)
+}
