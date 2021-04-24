607f0c301ad385181b7c59d3d4b816ec03568b6a
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-24 18:38:12 -0300

    refactor: mongodb uri

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://elciess:planeta05@elciess.ihcok.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://elciess:planeta05@elciess.ihcok.mongodb.net/elciess?retryWrites=true&w=majority',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || '3lc1355'
 }
