94fc6208a7c4c46751e2f697b69bf2e470d8ba78
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-24 18:10:49 -0300

    feat: ensure BodyParserMiddleware parse as json

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,9 +1,9 @@
-import { cors } from '@/main/middlewares'
+import { bodyParser ,cors } from '@/main/middlewares'
 
 import { Express } from 'express'
 
 export default (app: Express): void => {
-  // app.use(bodyParser)
+  app.use(bodyParser)
   app.use(cors)
   // app.use(contentType)
 }
