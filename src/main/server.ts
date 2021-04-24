8379f33aac19a492544decfaaf6663ea8e54b861
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-24 12:23:09 -0300

    feat: app and server config file

---
+++ src/main/server.ts
@@ -0,0 +1,10 @@
+import 'module-alias/register'
+import env from '@/main/config/env'
+import { MongoHelper } from '@/infra/db/mongodb'
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
