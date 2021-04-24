e137d3cc67c9a415c61fb481dc1ecce392ff6c72
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-24 18:21:52 -0300

    test: ensure ContentTypeMiddleware return xml content when forced

--- tests/main/middlewares/content-type.test.ts
+++ tests/main/middlewares/content-type.test.ts
@@ -11,4 +11,14 @@ describe('Content Type Middleware', () => {
       .get('/test_content_type')
       .expect('content-type', /json/)
   })
+
+  test('Should return xml content type when forced', async () => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send('')
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
 })
