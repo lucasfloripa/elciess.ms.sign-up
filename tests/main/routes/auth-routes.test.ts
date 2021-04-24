35583cdfd732f27894aca7d25afe661b949c15c9
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-24 18:25:00 -0300

    test: ensure AuthRotes call post /signup and returns 200

---
+++ tests/main/routes/auth-routes.test.ts
@@ -0,0 +1,36 @@
+import app from '@/main/config/app'
+import { MongoHelper } from '@/infra/db/mongodb'
+
+import { Collection } from 'mongodb'
+// import { hash } from 'bcrypt'
+import request from 'supertest'
+
+let userCollection: Collection
+
+describe('Auth Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    userCollection = await MongoHelper.getCollection('users')
+    await userCollection.deleteMany({})
+  })
+
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          email: 'lucasgoncalves@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+})
