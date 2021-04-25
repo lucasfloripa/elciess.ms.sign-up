a098be5fa3496ff22653dbd9a94f2dcdbaf95f79
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-25 10:22:14 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Elciess MS SignUp',
+    description: 'API to register users and to authenticate them in any other Elciess service.',
+    version: '1.0.0'
+  }
+}
