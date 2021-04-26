e331bab4a2c6ccd2b2e37fcf28e353d68840197d
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-26 17:10:31 -0300

    fix: fix test name

--- tests/presentation/controllers/authentication-controller.spec.ts
+++ tests/presentation/controllers/authentication-controller.spec.ts
@@ -20,7 +20,7 @@ const makeSut = (): SutTypes => {
 }
 
 describe('Authentication Controller', () => {
-  test('Should return 400 if Validation fails', async () => {
+  test('Should call Validation with correct values', async () => {
     const { sut, validationStub } = makeSut()
     const validateSpy = jest.spyOn(validationStub, 'validate')
     await sut.handle(mockRequest())
