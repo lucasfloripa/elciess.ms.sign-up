0cd12b3db1836cb4eadf968c42b06a29b97ad5b7
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-26 17:07:58 -0300

    feat: ensure AuthenticationController return 400 if Validation returns an error

--- src/presentation/controllers/authentication-controller.ts
+++ src/presentation/controllers/authentication-controller.ts
@@ -1,12 +1,16 @@
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
+import { badRequest } from '@/presentation/helpers'
 
 export class AuthenticationController implements Controller {
   constructor (
     private readonly validation: Validation
-  ) {}
+  ) { }
 
   async handle (request: AuthenticationController.Params): Promise<HttpResponse> {
-    this.validation.validate(request)
+    const error = this.validation.validate(request)
+    if (error) {
+      return badRequest(error)
+    }
     return null
   }
 }
