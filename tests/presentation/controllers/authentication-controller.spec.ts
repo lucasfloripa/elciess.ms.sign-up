16df646feb4f81277717a08c468d034b3df942e1
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-26 17:33:42 -0300

    test: ensure AuthenticationController call Authenticate with correct values

--- tests/presentation/controllers/authentication-controller.spec.ts
+++ tests/presentation/controllers/authentication-controller.spec.ts
@@ -1,7 +1,17 @@
 import { AuthenticationController } from '@/presentation/controllers'
 import { Validation } from '@/presentation/protocols'
 import { badRequest } from '@/presentation/helpers'
+import { Authenticate } from '@/domain/usecases'
 import { mockValidationStub } from '@/tests/utils/mocks'
+
+const mockAuthenticateStub = (): Authenticate => {
+  class AuthenticateStub implements Authenticate {
+    async auth (credentials: Authenticate.Params): Promise<string> {
+      return await Promise.resolve('access_token')
+    }
+  }
+  return new AuthenticateStub()
+}
 
 const mockRequest = (): AuthenticationController.Params => ({
   email: 'any_email@mail.com',
@@ -11,12 +21,14 @@ const mockRequest = (): AuthenticationController.Params => ({
 type SutTypes = {
   sut: AuthenticationController
   validationStub: Validation
+  authenticateStub: Authenticate
 }
 
 const makeSut = (): SutTypes => {
   const validationStub = mockValidationStub()
-  const sut = new AuthenticationController(validationStub)
-  return { sut, validationStub }
+  const authenticateStub = mockAuthenticateStub()
+  const sut = new AuthenticationController(validationStub, authenticateStub)
+  return { sut, validationStub, authenticateStub }
 }
 
 describe('Authentication Controller', () => {
@@ -32,5 +44,12 @@ describe('Authentication Controller', () => {
     jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
     const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual(badRequest(new Error()))
+  })
+
+  test('Should call Authenticate with correct values', async () => {
+    const { sut, authenticateStub } = makeSut()
+    const authSpy = jest.spyOn(authenticateStub, 'auth')
+    await sut.handle(mockRequest())
+    expect(authSpy).toHaveBeenCalledWith(mockRequest())
   })
 })
