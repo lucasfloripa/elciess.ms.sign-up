37a33a33e18c843fe3501f1a3c7abe3597a6887c
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-29 13:27:23 -0300

    feat: ensure AuthRoutes return 200 on POST /auth

--- src/main/routes/auth-routes.ts
+++ src/main/routes/auth-routes.ts
@@ -1,8 +1,9 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeSignUpController } from '@/main/factories/controllers'
+import { makeAuthenticationController, makeSignUpController } from '@/main/factories/controllers'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
+  router.post('/auth', adaptRoute(makeAuthenticationController()))
 }
