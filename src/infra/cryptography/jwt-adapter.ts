6c0d42ae23ef3e7460cb99fee3202c4bbb671936
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-04-29 13:20:37 -0300

    fix: jwt adapter sign param fix

--- src/infra/cryptography/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter.ts
@@ -4,6 +4,6 @@ import jwt from 'jsonwebtoken'
 
 export class JwtAdapter implements Encrypter {
   async encrypt (id: string): Promise<string> {
-    return jwt.sign(id, env.jwtSecret)
+    return jwt.sign({ id }, env.jwtSecret)
   }
 }
