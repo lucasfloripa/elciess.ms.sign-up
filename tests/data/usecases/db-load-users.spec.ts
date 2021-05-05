7ef8e370039ad7cdb1fc93353bc568ef64852c4a
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-05-05 17:22:49 -0300

    test: ensure DbLoadUsers return a list of users on success

--- tests/data/usecases/db-load-users.spec.ts
+++ tests/data/usecases/db-load-users.spec.ts
@@ -1,6 +1,7 @@
 import { DbLoadUsers } from '@/data/usecases'
 import { LoadUsersRepository } from '@/data/protocols'
 import { mockLoadUsersRepositoryStub } from '@/tests/data/mocks'
+import { mockListUserModel } from '@/tests/domain/mocks'
 
 type SutTypes = {
   sut: DbLoadUsers
@@ -21,10 +22,16 @@ describe('DbLoadUsers', () => {
     expect(loadAllSpy).toHaveBeenCalled()
   })
 
-  test('Should throw if loadUserByEmailRepository throws', async () => {
+  test('Should throw if loadUsersRepositoryStub throws', async () => {
     const { sut, loadUsersRepositoryStub } = makeSut()
     jest.spyOn(loadUsersRepositoryStub, 'loadAll').mockImplementationOnce(async () => await Promise.reject(new Error()))
     const promise = sut.load()
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return a list of users on success', async () => {
+    const { sut } = makeSut()
+    const users = await sut.load()
+    expect(users).toEqual(mockListUserModel())
   })
 })
