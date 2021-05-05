e1e72d7de88014a3d42882a4aa311cf874a1ca74
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-05-05 14:13:58 -0300

    feat: mockUserModel method

--- tests/domain/mocks/mock-user.ts
+++ tests/domain/mocks/mock-user.ts
@@ -1,6 +1,13 @@
 import { RegisterUser } from '@/domain/usecases'
+import { User } from '@/domain/models'
 
 export const mockRegisterUserParams = (): RegisterUser.Params => ({
   email: 'any_email@mail.com',
+  password: 'any_password'
+})
+
+export const mockUserModel = (): User => ({
+  id: 'any_id',
+  email: 'any_email',
   password: 'any_password'
 })
