c5f62c1fae3abdee3f12a99097b2df0b135dd249
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-05-05 18:25:08 -0300

    feat: getUsers route

--- src/main/routes/auth-routes.ts
+++ src/main/routes/auth-routes.ts
@@ -1,9 +1,10 @@
 import { adaptRoute } from '@/main/adapters'
-import { makeAuthenticationController, makeSignUpController } from '@/main/factories/controllers'
+import { makeAuthenticationController, makeSignUpController, makeLoadUsersController } from '@/main/factories/controllers'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
   router.post('/auth', adaptRoute(makeAuthenticationController()))
+  router.get('/users', adaptRoute(makeLoadUsersController()))
 }
