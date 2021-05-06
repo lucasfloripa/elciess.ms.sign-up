0062cc97e065c8a5df80daf656386d9052163391
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-05-06 16:19:52 -0300

    test: ensure AuthMiddleware return 403 if loadUserByToken returns null

--- tests/presentation/middlewares/auth-middleware.spec.ts
+++ tests/presentation/middlewares/auth-middleware.spec.ts
@@ -1,13 +1,27 @@
 import { AuthMiddleware } from '@/presentation/middlewares'
-import { unauthorized } from '@/presentation/helpers'
+import { forbidden, unauthorized } from '@/presentation/helpers'
+import { LoadUserByToken } from '@/domain/usecases'
+import { mockUserModel } from '@/tests/domain/mocks'
+import { AccessDeniedError } from '@/presentation/errors'
+
+const mockLoadUserByToken = (): LoadUserByToken => {
+  class LoadUserByTokenStub implements LoadUserByToken {
+    async loadByToken (token: string): Promise<LoadUserByToken.Result> {
+      return mockUserModel()
+    }
+  }
+  return new LoadUserByTokenStub()
+}
 
 type SutTypes = {
   sut: AuthMiddleware
+  loadUserByTokenStub: LoadUserByToken
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new AuthMiddleware()
-  return { sut }
+  const loadUserByTokenStub = mockLoadUserByToken()
+  const sut = new AuthMiddleware(loadUserByTokenStub)
+  return { sut, loadUserByTokenStub }
 }
 
 describe('Auth Middleware', () => {
@@ -15,5 +29,19 @@ describe('Auth Middleware', () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({ accessToken: '' })
     expect(httpResponse).toEqual(unauthorized())
+  })
+
+  test('Should call loadUserByToken with correct value', async () => {
+    const { sut, loadUserByTokenStub } = makeSut()
+    const loadUserByTokenSpy = jest.spyOn(loadUserByTokenStub, 'loadByToken')
+    await sut.handle({ accessToken: 'any_access_token' })
+    expect(loadUserByTokenSpy).toHaveBeenCalledWith('any_access_token')
+  })
+
+  test('Should return 403 if loadUserByToken returns null', async () => {
+    const { sut, loadUserByTokenStub } = makeSut()
+    jest.spyOn(loadUserByTokenStub, 'loadByToken').mockReturnValueOnce(null)
+    const httpResponse = await sut.handle({ accessToken: 'any_access_token' })
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
   })
 })
