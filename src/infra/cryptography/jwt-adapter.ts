cff57fc384e11cbdbdbdc345e0600b5ad7fe1ce0
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-05-07 21:31:51 -0300

    feat: ensure JwtAdapter calls decrypt with correct values

--- src/infra/cryptography/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter.ts
@@ -1,9 +1,13 @@
-import { Encrypter } from '@/data/protocols'
+import { Decrypter, Encrypter } from '@/data/protocols'
 import env from '@/main/config/env'
 import jwt from 'jsonwebtoken'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   async encrypt (id: string): Promise<string> {
     return jwt.sign({ id }, env.jwtSecret)
+  }
+
+  async decrypt (text: string): Promise<string> {
+    return jwt.verify(text, env.jwtSecret) as any
   }
 }
