9b27dc272fb55a532f6c4450adc6d9b4d52322ca
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-05-17 08:42:23 -0300

    feat: add admin auth to register and getUsers routes

--- src/main/routes/auth-routes.ts
+++ src/main/routes/auth-routes.ts
@@ -1,10 +1,11 @@
 import { adaptRoute } from '@/main/adapters'
 import { makeAuthenticationController, makeRegisterUserController, makeLoadUsersController } from '@/main/factories/controllers'
+import { adminAuth } from '@/main/middlewares/admin-auth'
 
 import { Router } from 'express'
 
 export default (router: Router): void => {
-  router.post('/register', adaptRoute(makeRegisterUserController()))
+  router.post('/register', adminAuth, adaptRoute(makeRegisterUserController()))
   router.post('/auth', adaptRoute(makeAuthenticationController()))
-  router.get('/users', adaptRoute(makeLoadUsersController()))
+  router.get('/users', adminAuth, adaptRoute(makeLoadUsersController()))
 }
