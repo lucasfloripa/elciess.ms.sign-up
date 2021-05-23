7840b9f63450a82d529c15064f41b1cf4a6058d9
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-05-23 16:19:22 -0300

    refactor: update RegisterUserRepository params

--- src/data/protocols/register-user-repository.ts
+++ src/data/protocols/register-user-repository.ts
@@ -1,10 +1,12 @@
-import { RegisterUser } from '@/domain/usecases'
-
 export interface RegisterUserRepository {
   register: (data: RegisterUserRepository.Params) => Promise<boolean>
 }
 
 export namespace RegisterUserRepository {
-  export type Params = RegisterUser.Params
+  export type Params = {
+    id: string
+    email: string
+    password: string
+  }
   export type Result = boolean
 }
