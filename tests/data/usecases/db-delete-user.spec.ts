c4e6479ba2c52635b4e3d9c96ab6b5176a090543
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-05-23 15:07:26 -0300

    test: ensure DbDeleteuser calls deleteUserByIdRepository with correct id

---
+++ tests/data/usecases/db-delete-user.spec.ts
@@ -0,0 +1,31 @@
+import { DbDeleteUser } from '@/data/usecases'
+import { DeleteUserByIdRepository } from '@/data/protocols'
+
+const mockDeleteUserByIdRepository = (): DeleteUserByIdRepository => {
+  class DeleteUserByIdRepositoryStub implements DeleteUserByIdRepository {
+    async deleteById (userId: string): Promise<boolean> {
+      return null
+    }
+  }
+  return new DeleteUserByIdRepositoryStub()
+}
+
+type SutTypes = {
+  deleteUserByIdRepository: DeleteUserByIdRepository
+  sut: DbDeleteUser
+}
+
+const makeSut = (): SutTypes => {
+  const deleteUserByIdRepository = mockDeleteUserByIdRepository()
+  const sut = new DbDeleteUser(deleteUserByIdRepository)
+  return { sut, deleteUserByIdRepository }
+}
+
+describe('DbDeleteUser Data Usecase', () => {
+  test('Should call deleteUserRepository with correct id', async () => {
+    const { sut, deleteUserByIdRepository } = makeSut()
+    const deleteByIdSpy = jest.spyOn(deleteUserByIdRepository, 'deleteById')
+    await sut.delete('any_id')
+    expect(deleteByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+})
