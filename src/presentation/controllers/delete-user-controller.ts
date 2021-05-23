9e6cbc5591a70151c9ac56255654e80f2fe5e2cf
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-05-23 14:30:11 -0300

    feat: ensure DeleteUserController call validation with correct value

--- src/presentation/controllers/delete-user-controller.ts
+++ src/presentation/controllers/delete-user-controller.ts
@@ -1,14 +1,16 @@
-import { Controller, HttpResponse } from '@/presentation/protocols'
+import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 import { notFound, serverError } from '@/presentation/helpers'
 import { DeleteUser } from '@/domain/usecases'
 
 export class DeleteUserController implements Controller {
   constructor (
+    private readonly validation: Validation,
     private readonly deleteUser: DeleteUser
   ) { }
 
   async handle (request: DeleteUserController.Params): Promise<HttpResponse> {
     try {
+      this.validation.validate(request)
       const exist = await this.deleteUser.delete(request.id)
       if (!exist) {
         return notFound()
