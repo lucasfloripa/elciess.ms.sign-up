fad91869129c00734e247e1c88ea998df248f49a
Lucas Gonçalves <lucas.floripa@icloud.com>
2021-07-22 14:38:41 -0300

    feat: doing

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,7 @@
+// elciess:planeta05@elciess.ihcok.mongodb.net/elciess
+
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/elciess.ms.auth-controll',
-  port: process.env.PORT || 6060,
+  port: process.env.PORT || 1010,
   jwtSecret: process.env.JWT_SECRET || '3lc1355'
 }
