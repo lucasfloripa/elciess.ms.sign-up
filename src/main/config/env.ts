5ae12d22f4c2d2b064f61e2e92fbec66d16562dd
Lucas Gonçalves <lucas.goncalves@calindra.com.br>
2021-07-28 09:03:42 -0300

    fix: fix the import path and add mongodb url to env file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,5 @@
-// elciess:planeta05@elciess.ihcok.mongodb.net/elciess
-
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/elciess.ms.auth-controll',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://elciess:planeta05@elciess.ihcok.mongodb.net/elciess?retryWrites=true&w=majority',
   port: process.env.PORT || 1010,
   jwtSecret: process.env.JWT_SECRET || '3lc1355'
 }
