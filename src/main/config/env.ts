0ca9a51b89baf5fcbf443570349f6b80f6449232
Lucas Gonçalves <lucas.floripa@icloud.com>
2022-04-17 09:14:35 -0300

    refactor: update env mongoUrl variable

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,7 @@
 // elciess:planeta05@elciess.ihcok.mongodb.net/elciess
 
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/elciess.ms.auth-controll',
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://elciess:planeta05@elciess.ihcok.mongodb.net/elciess',
   port: process.env.PORT || 1010,
   jwtSecret: process.env.JWT_SECRET || '3lc1355'
 }
